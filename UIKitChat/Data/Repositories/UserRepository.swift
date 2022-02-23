7ce4810ac63d2db462f621de214b27b5a2cb4849
yabby <yabby1997@gmail.com>
2022-02-23 22:09:37 +0900

    UserRepository 구현

---
+++ UIKitChat/Data/Repositories/UserRepository.swift
@@ -0,0 +1,27 @@
+//
+//  UserRepository.swift
+//  UIKitChat
+//
+//  Created by Seunghun Yang on 2022/02/21.
+//
+
+import Combine
+import Foundation
+
+class UserRepository: UserRepositoryProtocol {
+    
+    private let firebaseNetworkService: FirebaseNetworkServiceProtocol
+    
+    init(firebaseNetworkService: FirebaseNetworkServiceProtocol) {
+        self.firebaseNetworkService = firebaseNetworkService
+    }
+    
+    func authUser(uid: String, name: String, profileImage: Data?) -> AnyPublisher<Void, Error> {
+        let chattingUser = ChattingUser(name: name, profileImage: nil)
+        return firebaseNetworkService.setDocument(collection: FirebaseCollection.chattingUser, document: uid, transferable: chattingUser)
+    }
+    
+    func fetchUser(uid: String) -> AnyPublisher<ChattingUser, Error> {
+        return Just(ChattingUser(name: "", profileImage: URL(string: "https://naver.com")!)).setFailureType(to: Error.self).eraseToAnyPublisher()
+    }
+}
---
+++ UIKitChat/Domain/Interfaces/UserRepositoryProtocol.swift
@@ -0,0 +1,14 @@
+//
+//  UserRepositoryProtocol.swift
+//  UIKitChat
+//
+//  Created by Seunghun Yang on 2022/02/21.
+//
+
+import Combine
+import Foundation
+
+protocol UserRepositoryProtocol {
+    func authUser(uid: String, name: String, profileImage: Data?) -> AnyPublisher<Void, Error>
+    func fetchUser(uid: String) -> AnyPublisher<ChattingUser, Error>
+}
