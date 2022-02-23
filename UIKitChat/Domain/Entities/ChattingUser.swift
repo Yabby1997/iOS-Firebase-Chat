ef5766e4ba7c9ed708cffcd84488ca1926098129
yabby <yabby1997@gmail.com>
2022-02-23 22:08:06 +0900

    ChattingUser가 DataTransferable을 따르도록 변경

--- UIKitChat/Domain/Entities/ChattingUser.swift
+++ UIKitChat/Domain/Entities/ChattingUser.swift
@@ -7,7 +7,25 @@
 
 import Foundation
 
-struct ChattingUser {
+struct ChattingUser: DataTransferable {
     let name: String
-    let profileImage: URL
+    let profileImage: URL?
+    
+    init(name: String, profileImage: URL? = nil) {
+        self.name = name
+        self.profileImage = profileImage
+    }
+    
+    init?(dictionary: [String : Any]) {
+        guard let name = dictionary["name"] as? String else { return nil }
+        self.name = name
+        self.profileImage = dictionary["profileImage"] as? URL
+    }
+    
+    var dictionary: [String : Any] {
+        var dictionary: [String: Any] = [:]
+        dictionary["name"] = name
+        dictionary["profileImage"] = profileImage
+        return dictionary
+    }
 }
